c534a36eee8077c05bf246514ea006db398faa3f
samadhi1311 <samadhilakmina.sl@gmail.com>
2024-12-07 22:24:45 +0530

    Added password hash matching

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,4 +1,4 @@
-import { hashSync } from 'bcrypt-edge';
+import { hashSync, compareSync } from 'bcrypt-edge';
 
 /**
  * Hash a plain-text password.
@@ -11,4 +11,14 @@ import { hashSync } from 'bcrypt-edge';
  */
 export function hashPassword(password: string, saltRounds: number = 10) {
 	return hashSync(password, saltRounds);
+}
+
+/**
+ * Compare a plain-text password with a hashed password.
+ * @param {string} password - The plain-text password.
+ * @param {string} hashedPassword - The hashed password.
+ * @returns {boolean} - True if the password matches, false otherwise.
+ */
+export function verifyPassword(password: string, hashedPassword: string) {
+	return compareSync(password, hashedPassword);
 }
